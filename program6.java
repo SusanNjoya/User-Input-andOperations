b031f891b64bb480bb9ebea18cfa0cf135300d25
SusanNjoya <154384846+SusanNjoya@users.noreply.github.com>
2024-01-24 13:55:12 -0500

    Update and rename lab6.java to program6.java

--- program6.java
+++ program6.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class lab6{
+public class la6{
    public static void main (String []args)throws FileNotFoundException{
       Scanner inFile = new Scanner(new File("input.txt"));
       String text;

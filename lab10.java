793778ca94cdd1900aad6a0db613d6c99205c446
SusanNjoya <154384846+SusanNjoya@users.noreply.github.com>
2024-01-24 12:55:50 -0500

    Add files via upload

---
+++ lab10.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class lab10{
+   public static void main (String [] args){
+      
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.print("Enter a number");
+        int num1 = scanner.nextInt();
+
+        if (isEven(num1)) {
+          System.out.println("All integers are even.");
+        } 
+        
+          else {
+             System.out.println("All integers are not even.");
+          }
+    }
+      
+        }
+
+   public static boolean isEven(int num2){
+   
+    while (num2 > 0) {
+       
+         int digit = num2 % 10;
+           
+            if (digit % 2 != 0) {
+                
+                return false;
+                   }
+                      num2 /= 10;
+                }
+                return true;
+  }
+
+        }

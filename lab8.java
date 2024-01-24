ed8ae3e6d5a3079e5f641dec582b225ff10dc0a6
SusanNjoya <154384846+SusanNjoya@users.noreply.github.com>
2024-01-24 12:55:10 -0500

    Add files via upload

---
+++ lab8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class lab8{
+   public static void main(String[]args){
+   
+   int num;
+   int i; 
+   System.out.println("Enter # of contents of array");
+   try (Scanner console = new Scanner (System.in)) {
+    num = console.nextInt();
+       
+       int [] integers = new int[num];
+       
+       System.out.println("Enter contents of array");
+       for (i=0; i <num; i++){
+          integers[i]=console.nextInt();
+          break;
+       }
+      
+          for (i=num-1;i>=0; i--){
+          System.out.println("Contents of array in reverse are: " + integers[i]);
+          break;
+          }
+}
+   }
+}
